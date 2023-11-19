cc25719efaf13a9372e55fd60b7091e34be0a6f2
Tobias Fischer <tobiasfischer17@gmail.com>
2023-11-18 21:19:36 -0500

    decoder transformer model

--- diffusion.py
+++ diffusion.py
@@ -21,138 +21,72 @@ ITER_FOLDER    = "iteration_{0}"
 IMG_FILENAME   = "{0}_{1}.png"
 LOSS_FILENAME  = "graph_loss.png"
 ACCS_FILENAME  = "graph_acc.png"
-LOAD_COUNT     = 1
+LOAD_COUNT     = 20
 
 ITERATIONS  = 100000
-SAVE_EVERY  = 1000
-TEST_EVERY  = 50
+SAVE_EVERY  = 2000
+TEST_EVERY  = 100
 TEST_COUNT  = 1
 LR = 2**-13
 BS = 1
 
-DIM = 256*3
-N_HEADS = 12
-D_HEADS = 64
-CTX_LAYERS = 3
-START_SELF_ATTENTION = 3
-DEN_LAYERS = 4
-TIMESTEPS = 1000
+DIM     = 256
+FF_DIM  = DIM*2
+N_HEADS = 8
+DEC_LAYERS = 4
 
-VOCAB_SIZE = 1024
-FRAME_SIZE = 8*16
+BOS_TOKEN  = 1024
+VOCAB_SIZE = 1024 + 1
+FRAME_SIZE = 8*16 + 1
 CTX_FRAMES = 20
-CTX_SIZE   = CTX_FRAMES * FRAME_SIZE
+MAX_CTX_SIZE = CTX_FRAMES * FRAME_SIZE
 
-class CrossAttention:
-   def __init__(self, query_dim, context_dim, n_heads, d_head, dropout=0.1):
-      self.to_q = Linear(query_dim, n_heads*d_head, bias=False)
-      self.to_k = Linear(context_dim, n_heads*d_head, bias=False)
-      self.to_v = Linear(context_dim, n_heads*d_head, bias=False)
-      self.num_heads = n_heads
-      self.head_size = d_head
-      self.to_out = [Linear(n_heads*d_head, query_dim)]
+class TransformerBlock:
+   def __init__(self, embed_dim, num_heads, ff_dim, act=lambda x: x.relu(), dropout=0.1):
+      assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads"
+
+      self.num_heads = num_heads
+      self.head_size = embed_dim // num_heads
+      self.act = act
       self.dropout = dropout
 
-   def __call__(self, x, context=None):
-      context = x if context is None else context
-      q,k,v = self.to_q(x), self.to_k(context), self.to_v(context)
-      q,k,v = [y.reshape(x.shape[0], -1, self.num_heads, self.head_size).transpose(1,2) for y in (q,k,v)]
-      attention = Tensor.scaled_dot_product_attention(q, k, v).dropout(self.dropout).transpose(1,2)
-      h_ = attention.reshape(shape=(x.shape[0], -1, self.num_heads * self.head_size))
-      return h_.sequential(self.to_out)
+      self.query = (Tensor.scaled_uniform(embed_dim, embed_dim), Tensor.zeros(embed_dim))
+      self.key   = (Tensor.scaled_uniform(embed_dim, embed_dim), Tensor.zeros(embed_dim))
+      self.value = (Tensor.scaled_uniform(embed_dim, embed_dim), Tensor.zeros(embed_dim))
 
-class GEGLU:
-   def __init__(self, dim_in, dim_out):
-      self.proj = Linear(dim_in, dim_out * 2)
-      self.dim_out = dim_out
+      self.out = (Tensor.scaled_uniform(embed_dim, embed_dim), Tensor.zeros(embed_dim))
 
-   def __call__(self, x):
-      x, gate = self.proj(x).chunk(2, dim=-1)
-      return x * gate.gelu()
+      self.ff1 = (Tensor.scaled_uniform(embed_dim, ff_dim), Tensor.zeros(ff_dim))
+      self.ff2 = (Tensor.scaled_uniform(ff_dim, embed_dim), Tensor.zeros(embed_dim))
 
-class FeedForward:
-   def __init__(self, dim, mult=4):
-      self.net = [
-         GEGLU(dim, dim*mult),
-         lambda x: x,  # needed for weights loading code to work
-         Linear(dim*mult, dim)
-      ]
+      self.ln1 = (Tensor.ones(embed_dim), Tensor.zeros(embed_dim))
+      self.ln2 = (Tensor.ones(embed_dim), Tensor.zeros(embed_dim))
+
+   def attn(self, x):
+      # x: (bs, time, embed_dim) -> (bs, time, embed_dim)
+      query, key, value = [x.linear(*y).reshape(shape=(x.shape[0], -1, self.num_heads, self.head_size)).transpose(1,2) for y in [self.query, self.key, self.value]]
+      attention = Tensor.scaled_dot_product_attention(query, key, value, is_causal=True).transpose(1,2)
+      return attention.reshape(shape=(x.shape[0], -1, self.num_heads * self.head_size)).linear(*self.out)
 
    def __call__(self, x):
-      return x.sequential(self.net)
-
-class BasicTransformerBlock:
-   def __init__(self, dim, context_dim, n_heads, d_head, self_attend=True, dropout=0.1):
-      self.self_attend = self_attend
-      if self_attend:
-         self.norm1 = LayerNorm(dim)
-         self.attn1 = CrossAttention(dim, dim, n_heads, d_head)
-      self.norm2 = LayerNorm(dim)
-      self.attn2 = CrossAttention(dim, context_dim, n_heads, d_head)
-      self.norm3 = LayerNorm(dim)
-      self.ff = FeedForward(dim)
-      self.dropout = dropout
-
-   def __call__(self, x, context=None):
-      if self.self_attend:
-         x = self.attn1(self.norm1(x)) + x
-      x = self.attn2(self.norm2(x), context=context) + x
-      x = self.ff(self.norm3(x)).dropout(self.dropout) + x
+      x = x + self.attn(x).dropout(self.dropout)
+      x = x.layernorm().linear(*self.ln1)
+      x = x + self.act(x.linear(*self.ff1)).linear(*self.ff2).dropout(self.dropout)
+      x = x.layernorm().linear(*self.ln2)
       return x
 
-class PosTimeFusion:
-   def __init__(self, dim, hid_dim):
-      self.inp_emb = [
-         Linear(dim, hid_dim, bias=False),
-      ]
-      self.pos_emb = [
-         Linear(dim, hid_dim, bias=False),
-      ]
-      self.time_emb = [
-         Linear(dim, hid_dim, bias=False),
-      ]
-      self.out = [
-         Tensor.silu,
-         Linear(hid_dim, dim, bias=False),
-      ]
-   
-   def __call__(self, x:Tensor, pos:Tensor, time:Tensor) -> Tensor:
-      h = x.sequential(self.inp_emb) + pos.sequential(self.pos_emb) + time.sequential(self.time_emb)
-      return h.sequential(self.out) + x
+class CommaVQDecoder:
+   def __init__(self):
+      self.tok_embed = Embedding(VOCAB_SIZE, DIM)
+      self.pos_embed = Embedding(MAX_CTX_SIZE + FRAME_SIZE, DIM)
+      self.layers = [TransformerBlock(DIM, N_HEADS, FF_DIM) for _ in range(DEC_LAYERS)]
+      self.class_head = Linear(DIM, VOCAB_SIZE)
 
-class CommaVQDiffuser:
-   def __init__(self, vocab_size, ctx_size, ctx_layers, den_size, den_layers, dim, n_heads, d_heads):
-      self.ctx_tok_embed  = Embedding(vocab_size, dim)
-      self.ctx_pos_embed  = Embedding(ctx_size,   dim)
-      self.ctx_layer_norm = LayerNorm(dim)
-      self.ctx_layers = []
-      for _ in range(ctx_layers):
-         self.ctx_layers.append(BasicTransformerBlock(dim, dim, n_heads, d_heads))
-
-      self.den_size = den_size
-      self.den_pos_embed = Embedding(den_size, dim)
-      self.den_layers = []
-      for i in range(den_layers):
-         self.den_layers.append(BasicTransformerBlock(dim, dim, n_heads, d_heads, self_attend=(i+1>=START_SELF_ATTENTION)))
-      self.class_head = Linear(dim, vocab_size)
-
-   def make_context_from(self, x:Tensor) -> Tensor:
-      tok_emb = self.ctx_tok_embed(x)
-      pos_emb = self.ctx_pos_embed(Tensor.arange(0, x.shape[-1], requires_grad=False).reshape([1]*(len(x.shape)-1)+[x.shape[-1]]))
-      h = self.ctx_layer_norm(tok_emb + pos_emb)
-      return h.sequential(self.ctx_layers)
-   
-   def __call__(self, context:Tensor):
-      x = self.den_pos_embed(Tensor.arange(0, self.den_size, requires_grad=False).reshape((1,-1))).expand((context.shape[0],self.den_size,-1))
-      for layer in self.den_layers:
-         # if   isinstance(layer, PosTimeFusion):         x = layer(x, pos_emb, time_emb)
-         if isinstance(layer, BasicTransformerBlock): x = layer(x, context)
-         else: raise ValueError(f"got unsupported denoising layer {type(layer)}")
-      return self.class_head(x).log_softmax()
-MODEL_PARAMS = [VOCAB_SIZE, CTX_SIZE, CTX_LAYERS, FRAME_SIZE, DEN_LAYERS, DIM, N_HEADS, D_HEADS]
-
-
-
+   def __call__(self, x:Tensor) -> Tensor:
+      x = self.tok_embed(x) + self.pos_embed(Tensor.arange(0, x.shape[1], requires_grad=False).reshape((1,-1)))
+      x = x.sequential(self.layers)
+      x = self.class_head(x)
+      return x
 
 
 def _to_image(output):
@@ -177,8 +111,7 @@ def _save_decoded_image(output, filepath):
    plt.savefig(filepath)
 
 
-train_timesteps = np.arange(1, TIMESTEPS)
-test_timesteps = np.array(list(range(TIMESTEPS-1, 1, -TIMESTEPS//TEST_COUNT))[::-1])
+
 
 def save_test_snapshot(step, outputs, baselines, train_loss, test_loss, train_acc, test_acc):
    for i in range(1):
@@ -201,29 +134,6 @@ def save_test_snapshot(step, outputs, baselines, train_loss, test_loss, train_ac
    plt.legend()
    plt.savefig(os.path.join(SAVE_FOLDER, ACCS_FILENAME))
 
-# def save_test_snapshot_diffusion(step, outputs, baseline, train_loss, test_loss, train_accs, test_accs):
-#    baseline_path = os.path.join(SAVE_FOLDER, IMG_FILENAME.format('test_baseline', '0'))
-#    if not os.path.exists(baseline_path):
-#       _save_decoded_image(baseline, baseline_path)
-#    for i in range(TEST_COUNT):
-#       _save_decoded_image(outputs[i], os.path.join(SAVE_FOLDER, ITER_FOLDER.format(step), IMG_FILENAME.format('decoded_test', test_timesteps[i])))
-   
-#    plt.clf()
-#    plt.plot(np.arange(TEST_EVERY, len(train_loss)*TEST_EVERY+1, TEST_EVERY), train_loss, label='train')
-#    plt.plot(np.arange(TEST_EVERY, len(test_loss) *TEST_EVERY+1, TEST_EVERY), test_loss,  label='test')
-#    plt.ylim(0, None)
-#    plt.legend()
-#    plt.savefig(os.path.join(SAVE_FOLDER, LOSS_FILENAME))
-   
-#    plt.clf()
-#    for accs, label in [(train_accs, 'train'), (test_accs, 'test')]:
-#       x = np.arange(TEST_EVERY, len(accs[0])*TEST_EVERY+1, TEST_EVERY)
-#       for i in range(TEST_COUNT):
-#          y, l = accs[i], f"{label}_{test_timesteps[i]}"
-#          plt.plot(x, y, label=l)
-#    plt.ylim(0.0, 1.0)
-#    plt.legend()
-#    plt.savefig(os.path.join(SAVE_FOLDER, ACCS_FILENAME))
 
 def load_datasets():
    s_time = time.time()
@@ -246,8 +156,10 @@ def load_datasets():
 
 def train():
    np.random.seed(1337)
-   model = CommaVQDiffuser(*MODEL_PARAMS)
-   opt = Adam(get_parameters(model), LR)
+   model = CommaVQDecoder()
+   params = get_parameters(model)
+   print(f"{sum(prod(p.shape) for p in params)/1e6:.2f} million parameters")
+   opt = Adam(params, LR)
    
    train_datas, test_datas = load_datasets()
 
@@ -263,16 +175,18 @@ def train():
          data = train_datas[step % len(train_datas)]
          index = np.random.randint(data.shape[0] - CTX_FRAMES - 1)
 
-      prev_frames  = Tensor(data[index:index+CTX_FRAMES].reshape(1,-1), dtype=dtypes.float32, requires_grad=False)
-      target_frame = Tensor(data[index+CTX_FRAMES].reshape(1,-1), dtype=dtypes.float32)
+      frames = np.full((BS,MAX_CTX_SIZE+FRAME_SIZE), BOS_TOKEN, dtype=np.float32)
+      for i in range(CTX_FRAMES+1):
+         frames[0,1+i*FRAME_SIZE:(i+1)*FRAME_SIZE] = data[index].flatten()
 
-      context = model.make_context_from(prev_frames)
-      output  = model(context)
-      loss    = output.sparse_categorical_crossentropy(target_frame)
+      inputs  = Tensor(frames[:,:-1], dtype=dtypes.float32, requires_grad=False)
+      targets = Tensor(frames[:,-(FRAME_SIZE-1):], dtype=dtypes.float32)
+      output  = model(inputs)[:,-(FRAME_SIZE-1):]
+      loss    = output.sparse_categorical_crossentropy(targets)
 
       if is_test:
          test_loss.append(loss.numpy())
-         test_acc.append((output.argmax(axis=-1) == target_frame).mean().numpy())
+         test_acc.append((output.argmax(axis=-1) == targets).mean().numpy())
       else:
          loss.realize()
          opt.zero_grad()
@@ -280,14 +194,14 @@ def train():
          opt.step()
 
          train_loss.append(loss.numpy())
-         train_acc.append((output.argmax(axis=-1) == target_frame).mean().numpy())
+         train_acc.append((output.argmax(axis=-1) == targets).mean().numpy())
       
       if (step+1) % TEST_EVERY == 0:
          if is_test:
             step += 1
             print(f"Step {str(step): >5} | Train Loss: {sum(train_loss[-TEST_EVERY:])/TEST_EVERY:.4f} | Train Accuracy: {100.0*sum(train_acc[-TEST_EVERY:])/TEST_EVERY:.2f}% | Test Loss: {test_loss[-1]:.4f} | Test Accuracy: {100.0*test_acc[-1]:.2f}% | {(time.time() - s_time) / float(TEST_EVERY):.2f} sec/iter")
             s_time = time.time()
-            save_test_snapshot(step, output.argmax(axis=-1), target_frame, train_loss, test_loss, train_acc, test_acc)
+            save_test_snapshot(step, output.argmax(axis=-1), targets, train_loss, test_loss, train_acc, test_acc)
          is_test = not is_test
       else:
          step += 1
@@ -298,85 +212,11 @@ def train():
          safe_save(get_state_dict(model), os.path.join(SAVE_FOLDER, MODEL_FILENAME.format(step)))
 
 
-# def train_diffusion_model():
-#    np.random.seed(1337)
-#    model = CommaVQDiffuser(*MODEL_PARAMS)
-#    embed_classify = EmbedClassifyForDiffusion(*EMBED_CLASSIFY_PARAMS)
-#    load_state_dict(embed_classify, safe_load("weights/embed_classify/embed_classify_300.safetensor"))
-#    # sd = get_state_dict(model)
-#    opt = Adam(get_parameters(model), LR)
-   
-#    train_datas, test_datas = load_datasets()
-
-#    s_time = time.time()
-#    step, test_index = 0, 0
-#    train_loss, test_loss = [], []
-#    train_accs, test_accs = [[] for i in range(TEST_COUNT)], [[] for i in range(TEST_COUNT)]
-#    while step < ITERATIONS:
-#       if test_index < 2:
-#          data = train_datas[step % len(train_datas)]
-#          index = np.random.randint(data.shape[0] - CTX_FRAMES - 1)
-#       else:
-#          data = test_datas[1]
-#          index = 0
-#       size = BS if test_index == 0 else TEST_COUNT
-
-#       prev_frames = Tensor(data[index:index+CTX_FRAMES].reshape(1,-1), dtype=dtypes.float32, requires_grad=False)
-#       target_frame = Tensor(data[index+CTX_FRAMES].reshape(1,-1), dtype=dtypes.float32)
-#       context = model.make_context_from(prev_frames)
-
-#       # timesteps = np.random.randint(1, TIMESTEPS, size=(size,1)) if test_index == 0 else test_timesteps.reshape(-1,1)
-#       timesteps = np.random.choice(train_timesteps, size=(BS,1), replace=False) if test_index == 0 else test_timesteps.reshape(-1,1)
-#       alphas = Tensor(timesteps.astype(np.float32) / float(TIMESTEPS-1), dtype=dtypes.float32, requires_grad=False).reshape((-1,1,1))
-#       x_0 = embed_classify.make_x_0_from(target_frame).detach()
-#       x_t = ((1-alphas)*x_0 + alphas*Tensor.randn(*x_0.shape))
-
-#       e_t = model(x_t, context, Tensor(timesteps, dtype=dtypes.float32, requires_grad=False))
-#       pred_x_0 = x_t - e_t
-#       loss = (pred_x_0 - x_0).abs().sum() / prod(e_t.shape)
-#       output = embed_classify.estimate(pred_x_0)
-
-#       if test_index == 0:
-#          loss.realize()
-#          opt.zero_grad()
-#          loss.backward()
-#          opt.step()
-#       else:
-#          if test_index == 1:
-#             train_loss.append(loss.numpy())
-#             accs = train_accs
-#          else:
-#             test_loss.append(loss.numpy())
-#             accs = test_accs
-#          for i in range(TEST_COUNT):
-#             accs[i].append((output[i].argmax(axis=-1) == target_frame).mean().numpy())
-      
-#       if (step+1) % TEST_EVERY == 0:
-#          if test_index == 2:
-#             print(f"Step {str(step+1): >5} | Train Loss: {sum(train_loss[-1:]):.4f} | Train Accuracy: {100.0*sum(train_accs[i][-1] for i in range(TEST_COUNT))/TEST_COUNT:.2f}% | Test Loss: {test_loss[-1]:.4f} | Test Accuracy: {100.0*sum(train_accs[i][-1] for i in range(TEST_COUNT))/TEST_COUNT:.2f}% | {(time.time() - s_time) / float(TEST_EVERY):.2f} sec/iter")
-#             s_time = time.time()
-#             save_test_snapshot_diffusion(step+1, output.argmax(axis=-1), target_frame, train_loss, test_loss, train_accs, test_accs)
-
-#          test_index += 1
-#          if test_index > 2:
-#             test_index = 0
-#             step += 1
-#       else:
-#          step += 1
-
-#       if step % SAVE_EVERY == 0:
-#          if not os.path.exists(SAVE_FOLDER):
-#             os.makedirs(SAVE_FOLDER)
-#          safe_save(get_state_dict(model), os.path.join(SAVE_FOLDER, MODEL_FILENAME.format(step)))
-
-
-
-
 def generate(gen_count=50, iter=30000):
    Tensor.no_grad = True
 
    np.random.seed(1337)
-   model = CommaVQDiffuser(*MODEL_PARAMS)
+   model = CommaVQDecoder()
    load_state_dict(model, safe_load(os.path.join("weights", "2023_11_17_20_33_40_474544", MODEL_FILENAME.format(iter))))
 
    data = np.load(load_dataset("commaai/commavq", split='40[:2]')[0,0]['path'][0])
